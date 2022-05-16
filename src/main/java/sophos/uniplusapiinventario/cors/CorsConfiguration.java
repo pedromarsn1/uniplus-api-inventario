e42a6039896068abb1ac6427a4b677349cd88805
Pedro Nascimento <pedromarsn@gmail.com>
2022-05-16 13:01:26 -0300

    atualização 16/05/22

--- src/main/java/sophos/uniplusapiinventario/cors/CorsConfiguration.java
+++ src/main/java/sophos/uniplusapiinventario/cors/CorsConfiguration.java
@@ -24,7 +24,7 @@ public class CorsConfiguration implements WebMvcConfigurer, Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
         System.out.println("WebConfig; "+request.getRequestURI());
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
         response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
         response.setHeader("Access-Control-Max-Age", "3600");
@@ -41,8 +41,8 @@ public class CorsConfiguration implements WebMvcConfigurer, Filter {
             }
         } else {
             System.out.println("Pre-flight");
-            response.setHeader("Access-Control-Allow-Origin", "*");
-            response.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT");
+            response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+            response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE");
             response.setHeader("Access-Control-Max-Age", "3600");
             response.setHeader("Access-Control-Allow-Headers", "Access-Control-Expose-Headers"+"Authorization, content-type," +
                     "USERID"+"ROLE"+
